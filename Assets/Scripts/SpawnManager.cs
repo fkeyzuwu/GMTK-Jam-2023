be0f45195f052b1fbb8c50de3ae3c7e1ee774129
bvaturi <barak.v32@gmail.com>
2023-07-08 11:05:43 +0300

    Created A spawnTimer
    NewSpawn Timer Manager - only manage time and amount
    SpawnManager is now a singelton - manage only the spawns

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,41 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // An array of Spawnable gameobjects
-    public GameObject[] Spawnables;
-    public int maxWaitTime = 10;
-    public int minWaitTime = 2;
+    public static SpawnManager Instance;
 
     public int minOffsetSpawn = -10;
     public int maxOffsetSpawn =  10;
+
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnObject());
+        camera = Camera.main;
+        Instance = this;
     }
 
-    // Update is called once per frame
-
-    IEnumerator SpawnObject()
+    public void SpawnSpawnable(GameObject[] spawnables, int amount)
     {
-        // Need to create a random time wait
-        while (true)
+        for (int i = 0; i < amount; i++)
         {
-
-            // Vector3 randomSpotision3 = new Vector3(Random.Range(lowerLeftCornerMap.x, topRightCornerMap.x), Random.Range(lowerLeftCornerMap.y, topRightCornerMap.y),0f);
-            Vector3 randomSpotision3 = Camera.main.WorldToViewportPoint(transform.position);
+            Vector3 randomSpotision3 = camera.WorldToViewportPoint(transform.position);
 
             randomSpotision3.x = Mathf.Clamp01(randomSpotision3.x) + Random.Range(minOffsetSpawn, maxOffsetSpawn);
             randomSpotision3.y = Mathf.Clamp01(randomSpotision3.y) + Random.Range(minOffsetSpawn, maxOffsetSpawn);
 
-            
+            int randomIndex = Random.Range(0, spawnables.Length);
 
-            int randomIndex = Random.Range(0, Spawnables.Length);
-            
-            int randomWaitTile = Random.Range(minWaitTime, maxWaitTime);
-            Instantiate(Spawnables[randomIndex], randomSpotision3, Quaternion.identity);
-            yield return new WaitForSeconds(randomWaitTile);
+            Instantiate(spawnables[randomIndex], randomSpotision3, Quaternion.identity);
         }
-        
     }
 }
