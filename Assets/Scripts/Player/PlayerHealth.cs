ebcacb1074506a005ca4c6048271697c50aa25dc
fkeyz <fkeyzuwu@gmail.com>
2023-07-08 10:45:21 +0200

    added player health and TMPro

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    private int currentHealth;
+    [SerializeField] private TextMeshProUGUI healthText;
+    
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, currentHealth);
+        healthText.text = currentHealth.ToString();
+        if (currentHealth <= 0)
+        {
+            print("You win!");
+        }
+    }
+
+    public void Heal(int amount) 
+    {
+        currentHealth += amount;
+        healthText.text = currentHealth.ToString();
+    }
+}
