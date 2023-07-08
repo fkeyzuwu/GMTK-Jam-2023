f9cbc9317bdd360ffed50e9d2b08c434f7a4235e
bvaturi <barak.v32@gmail.com>
2023-07-08 03:13:57 +0300

    Updated spawn manager placement
    Ive used Camera.main.WorldToViewportPoint
    To get the relative view of the camera - and from there i've used a random range from the relative position of the camera view

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,11 @@ public class SpawnManager : MonoBehaviour
 {
     // An array of Spawnable gameobjects
     public GameObject[] Spawnables;
-    public int MaxWaitTime = 10;
-    public int MinWaitTime = 2;
-    public Vector2 topRightCornerMap;
-    public Vector2 lowerLeftCornerMap;
+    public int maxWaitTime = 10;
+    public int minWaitTime = 2;
+
+    public int minOffsetSpawn = -10;
+    public int maxOffsetSpawn =  10;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +24,20 @@ public class SpawnManager : MonoBehaviour
         // Need to create a random time wait
         while (true)
         {
+
+            // Vector3 randomSpotision3 = new Vector3(Random.Range(lowerLeftCornerMap.x, topRightCornerMap.x), Random.Range(lowerLeftCornerMap.y, topRightCornerMap.y),0f);
+            Vector3 randomSpotision3 = Camera.main.WorldToViewportPoint(transform.position);
+
+            randomSpotision3.x = Mathf.Clamp01(randomSpotision3.x) + Random.Range(minOffsetSpawn, maxOffsetSpawn);
+            randomSpotision3.y = Mathf.Clamp01(randomSpotision3.y) + Random.Range(minOffsetSpawn, maxOffsetSpawn);
+
+            
+
             int randomIndex = Random.Range(0, Spawnables.Length);
-            Vector2 randomSpotision = new Vector2(Random.Range(lowerLeftCornerMap.x, topRightCornerMap.x), Random.Range(lowerLeftCornerMap.y, topRightCornerMap.y));
-            int RandomWaitTile = Random.Range(MinWaitTime, MaxWaitTime);
-            Instantiate(Spawnables[randomIndex], randomSpotision, Quaternion.identity);
-            yield return new WaitForSeconds(RandomWaitTile);
+            
+            int randomWaitTile = Random.Range(minWaitTime, maxWaitTime);
+            Instantiate(Spawnables[randomIndex], randomSpotision3, Quaternion.identity);
+            yield return new WaitForSeconds(randomWaitTile);
         }
         
     }
