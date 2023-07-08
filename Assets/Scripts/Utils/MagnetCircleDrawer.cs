5846e8cae31526734c717c336a096e79c122cb91
fkeyz <fkeyzuwu@gmail.com>
2023-07-08 23:43:34 +0200

    collider fix

--- Assets/Scripts/Utils/MagnetCircleDrawer.cs
+++ Assets/Scripts/Utils/MagnetCircleDrawer.cs
@@ -8,6 +8,7 @@ public class MagnetFieldDrawer : MonoBehaviour
     [SerializeField] CircleCollider2D collider;
     public void UpgradeMagnetField()
     {
+        collider.enabled = true;
         collider.radius += 1;
         magnetField.enabled = true;
         magnetField.transform.localScale = new Vector3(collider.radius * 2, collider.radius * 2, collider.radius * 2);
