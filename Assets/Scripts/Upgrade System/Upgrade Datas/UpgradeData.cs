781c86bf87567eb16f1f1a38c32149c9fa07604c
babandrey <bab.andrey@gmail.com>
2023-07-09 12:22:08 +0300

    Update UpgradeData.cs
    Re-ordering to make the Unity Editor nicer

--- Assets/Scripts/Upgrade System/Upgrade Datas/UpgradeData.cs
+++ Assets/Scripts/Upgrade System/Upgrade Datas/UpgradeData.cs
@@ -4,13 +4,13 @@ public abstract class UpgradeData : ScriptableObject
 {
     public string Name;
     public string Description;
+    public float Duration;
+    public int MaxStack;
     public Sprite Icon;
     public bool IsTimed;
-    public float Duration;
     public bool IsStackable;
     public bool IsDurationExtending;
     public bool IsMaxStackLimited;
-    public int MaxStack;
 
     public abstract Upgrade InitializeUpgrade(GameObject obj);
 }
