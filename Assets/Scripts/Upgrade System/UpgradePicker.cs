81029b590f0c262f337fa774a4b8a053831b9a64
babandrey <bab.andrey@gmail.com>
2023-07-09 12:14:31 +0300

    Unique Numbers Generator
    Added a random number generator that generates unique numbers.
    If the requested amount is less than whats available, the last number will be just be duplicated.

--- Assets/Scripts/Upgrade System/UpgradePicker.cs
+++ Assets/Scripts/Upgrade System/UpgradePicker.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class UpgradePicker : MonoBehaviour
 {
     public UpgradePick[] upgradePicks;
+
     public void GenerateUpgrades()
     {
         UpgradeSystem upgradeSystem = PlayerController.Instance.upgradeSystem;
         List<UpgradeData> upgrades = upgradeSystem.GenerateRandomUpgrades(3);
+
+        if (upgrades == null)
+        {
+            Debug.LogError("[UpgradePicker] Failed to receive a list of upgrades");
+            return;
+        }
+
         for (int i = 0; i < upgrades.Count; i++)
         {
             upgradePicks[i].UpdatePick(upgrades[i]);
